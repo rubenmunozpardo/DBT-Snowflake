af26597031fa59a815f1182e70e8a0b8b24d21a7
ruben munoz <ruben.munoz.pardo@altia.es>
2025-01-08 13:15:01 +0000

    dx

--- models/D/vista_cliente.sql
+++ models/D/vista_cliente.sql
@@ -1,14 +1,59 @@
-SELECT 
-    C.C_CUSTKEY AS CLIENTE_ID,
-    SUM(L.L_EXTENDEDPRICE * M.TASA_CAMBIO) AS TOTAL_GASTO_MONEDA_LOCAL,
-    COUNT(E.ID_EVENTO) AS NUMERO_EVENTOS
-FROM 
-    {{ ref('customer') }} C
-JOIN 
-    {{ ref('lineitem') }} L ON C.C_CUSTKEY = L.L_CUSTKEY
-JOIN 
-    {{ ref('moneda_local') }} M ON L.L_ORDERKEY = M.O_ORDERKEY
-LEFT JOIN 
-    {{ ref('evento') }} E ON L.L_ORDERKEY = E.O_ORDERKEY
-GROUP BY 
-    C.C_CUSTKEY;
+-- models/vista_cliente.sql
+
+WITH customer_data AS (
+    SELECT
+        C_CUSTKEY,
+        C_NAME,
+        C_ADDRESS,
+        C_PHONE,
+        C_ACCTBAL
+    FROM
+        DBT_PROJECT.CUSTOMER
+),
+lineitem_data AS (
+    SELECT
+        L_ORDERKEY,
+        L_PARTKEY,
+        L_SUPPKEY,
+        L_QUANTITY,
+        L_EXTENDEDPRICE,
+        L_DISCOUNT,
+        L_TAX,
+        L_RETURNFLAG,
+        L_LINESTATUS,
+        L_SHIPDATE,
+        L_COMMITDATE,
+        L_RECEIPTDATE,
+        L_SHIPINSTRUCT,
+        L_SHIPMODE,
+        L_COMMENT
+    FROM
+        DBT_PROJECT.LINEITEM
+)
+SELECT
+    l.L_ORDERKEY,
+    l.L_PARTKEY,
+    l.L_SUPPKEY,
+    l.L_QUANTITY,
+    l.L_EXTENDEDPRICE,
+    l.L_DISCOUNT,
+    l.L_TAX,
+    l.L_RETURNFLAG,
+    l.L_LINESTATUS,
+    l.L_SHIPDATE,
+    l.L_COMMITDATE,
+    l.L_RECEIPTDATE,
+    l.L_SHIPINSTRUCT,
+    l.L_SHIPMODE,
+    l.L_COMMENT,
+    c.C_CUSTKEY,
+    c.C_NAME,
+    c.C_ADDRESS,
+    c.C_PHONE,
+    c.C_ACCTBAL
+FROM
+    lineitem_data l
+JOIN
+    customer_data c
+ON
+    l.L_ORDERKEY = c.C_CUSTKEY
--- models/D/vista_tienda.sql
+++ models/D/vista_tienda.sql
@@ -1,14 +1,24 @@
+-- models/vista_tienda.sql
+{{ config(materialized="view") }}
 
+with
+    ventas_moneda_local as (
+        select l.l_suppkey, l.l_extendedprice * m.tasa_cambio as venta_moneda_local
+        from {{ ref("lineitem") }} l
+        join {{ ref("moneda_local") }} m on l.l_orderkey = m.o_orderkey
+    ),
+    eventos_por_tienda as (
+        select l.l_suppkey, count(e.id_evento) as numero_eventos
+        from {{ ref("lineitem") }} l
+        left join {{ ref("evento") }} e on l.l_orderkey = e.o_orderkey
+        group by l.l_suppkey
+    )
 
-SELECT 
-    L.L_SUPPKEY AS TIENDA_ID,
-    SUM(L.L_EXTENDEDPRICE * M.TASA_CAMBIO) AS TOTAL_VENTAS_MONEDA_LOCAL,
-    COUNT(E.ID_EVENTO) AS NUMERO_EVENTOS
-FROM 
-    {{ ref('LINEITEM') }} L
-JOIN 
-    {{ ref('MONEDA_LOCAL') }} M ON L.L_ORDERKEY = M.O_ORDERKEY
-LEFT JOIN 
-    {{ ref('EVENTO') }} E ON L.L_ORDERKEY = E.O_ORDERKEY
-GROUP BY 
-    L.L_SUPPKEY
+select
+    v.l_suppkey as tienda_id,
+    sum(v.venta_moneda_local) as total_ventas_moneda_local,
+    e.numero_eventos
+from ventas_moneda_local v
+join eventos_por_tienda e on v.l_suppkey = e.l_suppkey
+group by v.l_suppkey, e.numero_eventos
+
