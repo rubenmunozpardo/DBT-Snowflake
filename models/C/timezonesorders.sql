aa3c226feb5582f921f675bdc1e2003e98e2e0bb
ruben munoz <ruben.munoz.pardo@altia.es>
2025-01-08 09:48:00 +0000

    gcjg

--- models/C/timezonesorders.sql
+++ models/C/timezonesorders.sql
@@ -1,16 +1,22 @@
--- models/order_country_utc.sql
-
 WITH orders AS (
     SELECT 
         o.O_ORDERKEY, 
-        o.O_PURCHASEDATETIME, 
         n.N_NAME AS COUNTRY, 
         t.TIMEZONE AS ORDER_TIMEZONE, 
-        t.UTC_OFFSET
-    FROM {{ ref('ORDERS') }} o
-    JOIN {{ ref('CUSTOMER') }} c ON o.O_CUSTKEY = c.C_CUSTKEY
-    JOIN {{ ref('NATION') }} n ON c.C_NATIONKEY = n.N_NATIONKEY
-    JOIN {{ ref('TIMEZONEs') }} t ON n.N_NATIONKEY = t.N_NATIONKEY
+        DATEADD('second', 
+                (CAST(SUBSTR(t.UTC_OFFSET, 2, 2) AS INT) * 3600) + 
+                (CAST(SUBSTR(t.UTC_OFFSET, 5, 2) AS INT) * 60) * 
+                CASE WHEN SUBSTR(t.UTC_OFFSET, 1, 1) = '+' THEN 1 ELSE -1 END, 
+                TO_TIMESTAMP(o.O_PURCHASEDATETIME)) AS FORMATTED_ORDER_TIME
+    FROM {{ ref('orders') }} o
+    JOIN {{ ref('customer') }} c ON o.O_CUSTKEY = c.C_CUSTKEY
+    JOIN {{ ref('nation') }} n ON c.C_NATIONKEY = n.N_NATIONKEY
+    JOIN {{ ref('timezones') }} t ON n.N_NAME = t.N_NAME
 )
 
-SELECT * FROM orders;
+SELECT 
+    O_ORDERKEY, 
+    COUNTRY, 
+    ORDER_TIMEZONE, 
+    FORMATTED_ORDER_TIME 
+FROM orders
